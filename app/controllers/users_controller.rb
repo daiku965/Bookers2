158a81240ce0a207bfe2bc84b0ac084839d8304b
daiku965 <daiku965@gmail.com>
2021-09-09 08:45:47 +0000

    [Add] users/showページ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
   def edit
   end
 end
