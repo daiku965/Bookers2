92d69f114f2f3b11ead7834025df14aa6128df78
daiku965 <daiku965@gmail.com>
2021-09-10 00:57:36 +0000

    [Add] 部分テンプレート

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'homes/about' => 'homes#about'
-  resources :books, only: [:create, :index, :show, :edit, :destroy]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
   end
