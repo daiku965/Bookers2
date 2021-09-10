fef324c79323781039d75101d77f62287f75bd3c
daiku965 <daiku965@gmail.com>
2021-09-10 02:18:27 +0000

    [Add] Update機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '35b6162d94e2da46e8aa06eebd39b56ac1ba2ca49ffebccad45cd230389fb1cdb5c678735ed90dbacd9607c5a1cadefa166520abdfd278efb8a31d63ab7c1d15'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homes/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
   end
