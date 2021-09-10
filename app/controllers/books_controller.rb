1bf91b14ffebe763ccf6e2589e4d1f86e30952e6
daiku965 <daiku965@gmail.com>
2021-09-10 06:19:22 +0000

    [Add] 投稿時のバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,18 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-    if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
     end
   end
 
