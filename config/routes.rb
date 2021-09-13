044969eeacc3cb1e555eae39107978266741d347
daiku965 <daiku965@gmail.com>
2021-09-13 09:34:37 +0000

    [Add] いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' => 'homes#about'
-  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: [:index, :show, :edit, :update]
+end
