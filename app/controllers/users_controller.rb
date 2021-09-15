36cb448be3188bf7972d69f192accac63d8b055b
daiku965 <daiku965@gmail.com>
2021-09-15 04:28:17 +0000

    [Fix] User系のコードレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @book = Book.new
   end
 
   def edit
@@ -30,7 +31,7 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
   def ensure_current_user
     @user = User.find(params[:id])
     unless @user == current_user
