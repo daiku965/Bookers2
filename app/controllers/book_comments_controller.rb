762a74675030e56535aa8827f6e18c8e07f2af1b
daiku965 <daiku965@gmail.com>
2021-09-19 04:11:12 +0000

    [Fix] コメント機能のajax化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,15 +5,15 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    @book_comment.save
-    #redirect_to request.referer
+    unless @book_comment.save
+      render 'error'
+    end
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    book_comment = BookComment.find(params[:id])
+    book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
-    #redirect_to request.referer
   end
 
   private
